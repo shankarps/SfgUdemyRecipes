10b062efa0009977bd19f281e90a1f223326aefb
shankar.pachari <shankar.pachari@gmail.com>
2021-01-14 09:58:17 -0800

    Added Mock Web MVC test case

---
+++ src/test/java/com/shankar/sfg/recipes/recipes/controllers/IndexControllerMVCTest.java
@@ -0,0 +1,48 @@
+package com.shankar.sfg.recipes.recipes.controllers;
+
+import com.shankar.sfg.recipes.recipes.domain.Recipe;
+import com.shankar.sfg.recipes.recipes.services.RecipeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(IndexController.class)
+public class IndexControllerMVCTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private RecipeService service;
+
+    @Test
+    void tetIndexControllerMVC() throws Exception {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(2L);
+
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+        when(service.getAllRecipes()).thenReturn(recipes);
+
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+        .andExpect(view().name("index"));
+    }
+}
--- src/test/java/com/shankar/sfg/recipes/recipes/controllers/IndexControllerTest.java
+++ src/test/java/com/shankar/sfg/recipes/recipes/controllers/IndexControllerTest.java
@@ -1,19 +1,23 @@
 package com.shankar.sfg.recipes.recipes.controllers;
 
+import com.shankar.sfg.recipes.recipes.domain.Recipe;
 import com.shankar.sfg.recipes.recipes.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class IndexControllerTest {
@@ -30,13 +34,33 @@ class IndexControllerTest {
     @BeforeEach
     public void setup(){
         System.out.println("Setup");
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(2L);
+
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+        when(recipeService.getAllRecipes()).thenReturn(recipes);
     }
 
     @Test
     void getIndexPage() {
+
         String st = indexController.getIndexPage(model);
         assertEquals(st, "index");
+        //verify that the recipeService.getAllRecipes() method is called once
         verify(recipeService, times(1)).getAllRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+
+        //Arg captor is needed to capture an arg of a mocked object.
+        ArgumentCaptor<Set<Recipe>> recipeArgCaptor = ArgumentCaptor.forClass(Set.class);
+        //verify that model.addAttriute() is called, to et attribute "recipes"
+        //Also capture the set value in captor.
+        verify(model, times(1))
+                .addAttribute(eq("recipes"), recipeArgCaptor.capture());
+        assertEquals(recipeArgCaptor.getValue().size(), 2);
+
     }
 }
